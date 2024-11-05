af4eb528f8e36ec0a7d9bfdf8c0d11bdb6bd98fc
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2024-11-06 01:04:01 +0900

    sdk 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.composelab"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.composelab"
         minSdk = 25
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
